27477aa94f05a3005438e45733f770dbec55b680
yoniwitz <jonhirsh39@gmail.com>
2021-02-17 16:21:04 -0500

    added product detail component

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from '../products/IProduct';
+@Component({
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

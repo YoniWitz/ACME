b9b5bdf56fbea098610253eb13870066e7a0c8cd
yoniwitz <jonhirsh39@gmail.com>
2021-02-18 15:20:25 -0500

    added product detail link

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {IProduct} from '../products/IProduct';
 @Component({
   templateUrl: './product-details.component.html',
@@ -7,9 +8,12 @@ import {IProduct} from '../products/IProduct';
 export class ProductDetailsComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    
+   }
 
   ngOnInit(): void {
+    let id: string = this.route.snapshot.paramMap.get('id');
   }
 
 }

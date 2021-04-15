c33a741dd013eceae7b0a56934d8159a8e937367
yoniwitz <jonhirsh39@gmail.com>
2021-04-15 19:39:43 -0400

    make edit product reactive

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductDetailsGuard } from './product-details/product-details.guard';
 
-import {SharedModule} from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { StarComponent } from '../shared/star/star.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 
@@ -27,16 +28,17 @@ let arrayOfRoutes: Route[] = [
   declarations: [
     ProductListComponent,
     ProductDetailsComponent,
-     
+
     StarComponent,
-   
+
     ConvertToSpacesPipe,
-   
-    ProductEditComponent,  
+
+    ProductEditComponent,
   ],
   imports: [
     RouterModule.forChild(arrayOfRoutes),
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
